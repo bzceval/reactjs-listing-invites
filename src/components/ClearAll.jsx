6485e80b3429e0adb6b0b301723eeaeb1e7e6a78
bzceval <busraaceval@gmail.com>
2022-12-03 18:23:59 +0300

    add icon hover animation effect

--- src/components/ClearAll.jsx
+++ src/components/ClearAll.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { GiPhone, GiAges } from 'react-icons/gi'; 
-import { RiLockPasswordFill } from 'react-icons/ri';
-import { MdMarkEmailUnread, MdOutlineLocationCity } from 'react-icons/md';
-import { FaUserSecret } from 'react-icons/fa';
+import { GiPhone, GiAges } from "react-icons/gi";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { MdMarkEmailUnread, MdOutlineLocationCity } from "react-icons/md";
+import { FaUserSecret } from "react-icons/fa";
 
 const ClearAll = () => {
   const [user, setUser] = useState();
@@ -21,15 +21,20 @@ const ClearAll = () => {
       <p>Click to View All Invite List.</p>
       <div className="row">
         <div className="col">
-          <div className="card user-card rounded-3 shadow-sm">
-           <div className="card-body d-flex gap-5">
-            <FaUserSecret className="icon p-1 m-1 rounded-5" />
-            <GiAges className="icon p-1 m-1 rounded-5" />
-            <MdMarkEmailUnread className="icon p-1 m-1 rounded-5" />
-            <GiPhone className="icon p-1 m-1 rounded-5" />
-            <MdOutlineLocationCity className="icon p-1 m-1 rounded-5" />
-            <RiLockPasswordFill className="icon p-1 m-1 rounded-5" />
-           </div>
+          <div className="card user-card rounded-3 shadow">
+            
+            <div className="card-body d-flex gap-5">
+              <FaUserSecret className="icon p-1 rounded-5" />
+              <GiAges className="icon p-1 rounded-5" />
+              <MdMarkEmailUnread className="icon p-1 rounded-5" />
+              <GiPhone className="icon p-1 rounded-5" />
+              <MdOutlineLocationCity className="icon p-1 rounded-5" />
+              <RiLockPasswordFill className="icon p-1 rounded-5" />
+            </div>
+            <div className="d-flex justify-content-around my-3">
+              <button className="btn btn-outline-dark">get user</button>
+              <button className="btn btn-outline-dark">add user</button>
+            </div>
           </div>
         </div>
       </div>
