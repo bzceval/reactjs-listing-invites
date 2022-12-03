6a43a20cbebb8c15a4bb91ea797d2293fb2c6b3b
bzceval <busraaceval@gmail.com>
2022-12-03 22:26:56 +0300

    change button color and backgroınd

--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 
 const PeopleList = () => { 
   const {addUser} = useContext(UserContext)
+
   return (
     <div className="people-container">
       <h1>People List</h1> 
