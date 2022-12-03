10031096955b734397a6569722b9d2abf065d333
bzceval <busraaceval@gmail.com>
2022-12-03 20:29:59 +0300

    add dynamic mouse events function

--- src/components/ClearAll.jsx
+++ src/components/ClearAll.jsx
@@ -4,8 +4,19 @@ import { RiLockPasswordFill } from "react-icons/ri";
 import { MdMarkEmailUnread, MdOutlineLocationCity } from "react-icons/md";
 import { FaUserSecret } from "react-icons/fa";
 
+const initialState = {
+  name: false,
+  gender: false,
+  email: false,
+  age: false,
+  location: false,
+  phone: false,
+  login: false,
+};
 const ClearAll = () => {
   const [user, setUser] = useState();
+  const [hoverAttribute, setHoverAttribute] = useState(initialState);
+
   const getUser = async () => {
     const url = `https://randomuser.me/api`;
     try {
@@ -15,24 +26,33 @@ const ClearAll = () => {
     } catch (error) {
       console.log(error);
     }
-  };
+  }; 
+
   return (
     <div className="clear-container">
       <p>Click to View All Invite List.</p>
       <div className="row">
         <div className="col">
           <div className="card user-card rounded-3 shadow">
-            
-            <div className="card-body d-flex gap-5">
-              <FaUserSecret className="icon p-1 rounded-5" />
-              <GiAges className="icon p-1 rounded-5" />
-              <MdMarkEmailUnread className="icon p-1 rounded-5" />
-              <GiPhone className="icon p-1 rounded-5" />
-              <MdOutlineLocationCity className="icon p-1 rounded-5" />
-              <RiLockPasswordFill className="icon p-1 rounded-5" />
+            <div className="card-main my-3">
+            {hoverAttribute.name && <p>Busra</p>}  
+            {hoverAttribute.email && <p>@gmail</p>} 
+            {hoverAttribute.age && <p>24</p>} 
+            {hoverAttribute.phone && <p>5236698</p>} 
+            {hoverAttribute.location && <p>Gazi</p>}
+            {hoverAttribute.info && <p>yes</p>}
+            </div>
+
+            <div className="card-body d-flex">
+              <FaUserSecret className="icon p-2 rounded-5 mx-4"/>
+              <GiAges className="icon p-2 mx-4 rounded-5" onMouseEnter={()=> setHoverAttribute({...hoverAttribute, name: true})} onMouseLeave={()=> setHoverAttribute({...hoverAttribute, name: false})} />
+              <MdMarkEmailUnread className="icon p-2 mx-4 rounded-5" onMouseEnter={()=> setHoverAttribute({...hoverAttribute, email: true})} onMouseLeave={()=> setHoverAttribute({...hoverAttribute, email: false})}/>
+              <GiPhone className="icon p-2 mx-4 rounded-5" onMouseEnter={()=> setHoverAttribute({...hoverAttribute, phone: true})} onMouseLeave={()=> setHoverAttribute({...hoverAttribute, phone: false})} />
+              <MdOutlineLocationCity className="icon p-2 mx-4 rounded-5" onMouseEnter={()=> setHoverAttribute({...hoverAttribute, location: true})} onMouseLeave={()=> setHoverAttribute({...hoverAttribute, location: false})} />
+              <RiLockPasswordFill className="icon p-2 mx-4 rounded-5" onMouseEnter={()=> setHoverAttribute({...hoverAttribute, info: true})} onMouseLeave={()=> setHoverAttribute({...hoverAttribute, info: false})} />
             </div>
             <div className="d-flex justify-content-around my-3">
-              <button className="btn btn-outline-dark">get user</button>
+              <button className="btn btn-outline-dark" onClick={getUser}>get user</button>
               <button className="btn btn-outline-dark">add user</button>
             </div>
           </div>
