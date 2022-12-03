a146cbeae97ae1eee069d5050975d46c154d034f
bzceval <busraaceval@gmail.com>
2022-12-03 18:06:39 +0300

    add random user card icon

--- src/components/ClearAll.jsx
+++ src/components/ClearAll.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
+import { GiPhone, GiAges } from 'react-icons/gi'; 
+import { RiLockPasswordFill } from 'react-icons/ri';
+import { MdMarkEmailUnread, MdOutlineLocationCity } from 'react-icons/md';
+import { FaUserSecret } from 'react-icons/fa';
 
 const ClearAll = () => {
-  const [user, setUser] = useState()
+  const [user, setUser] = useState();
   const getUser = async () => {
     const url = `https://randomuser.me/api`;
     try {
       const res = await fetch(url);
       const data = await res.json();
-      setUser(data.results[0])
+      setUser(data.results[0]);
     } catch (error) {
       console.log(error);
     }
@@ -17,9 +21,15 @@ const ClearAll = () => {
       <p>Click to View All Invite List.</p>
       <div className="row">
         <div className="col">
-          <div className="card rounded-3 shadow-sm">
-            <p className="text-dark">{user?.gender}</p>
-            <img src={user?.picture.medium} alt="" />
+          <div className="card user-card rounded-3 shadow-sm">
+           <div className="card-body d-flex gap-5">
+            <FaUserSecret className="icon p-1 m-1 rounded-5" />
+            <GiAges className="icon p-1 m-1 rounded-5" />
+            <MdMarkEmailUnread className="icon p-1 m-1 rounded-5" />
+            <GiPhone className="icon p-1 m-1 rounded-5" />
+            <MdOutlineLocationCity className="icon p-1 m-1 rounded-5" />
+            <RiLockPasswordFill className="icon p-1 m-1 rounded-5" />
+           </div>
           </div>
         </div>
       </div>
